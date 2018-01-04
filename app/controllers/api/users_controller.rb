2c93263799225909684aa7e94ca9f0d56e9b3ae8
Julie <julie.lin0812@gmail.com>
2018-01-04 01:11:16 -0800

    Adds users route & controller method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,17 @@
 class Api::UsersController < ApplicationController
-  def new
+  def create
+    @user = new User.new(user_params)
+    if @user.save
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
-  def create
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'users/new'
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: %i(create)
+    resource :session, only: %i(create destroy)
   end
-
-  namespace :api do
-    get 'users/create'
-  end
-
-  get 'user/new'
-
-  get 'user/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
