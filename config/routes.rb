b9352ec6cc42e489b17336062039036c2ee3f02f
Julie <julie.lin0812@gmail.com>
2018-01-04 01:03:36 -0800

    Initial commit of users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users/new'
+  end
+
+  namespace :api do
+    get 'users/create'
+  end
+
+  get 'user/new'
+
+  get 'user/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
 end
